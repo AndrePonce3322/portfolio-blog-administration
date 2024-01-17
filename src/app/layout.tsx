d9e28cf32a42420d08d5fcbec31d45b4d49342c3
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-01-16 19:05:34 -0600

    update: layout title in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import Background from '@/components/background';
+import { Footer } from '@/components/footer';
 import MyBlogProvider from '@/context/blog-context';
 import type { Metadata } from 'next';
 import { Onest } from 'next/font/google';
 import { Toaster } from 'sonner';
 import './globals.css';
-import { Footer } from '@/components/footer';
 
 const onest = Onest({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Administración de Blog',
+  title: 'André P. García | Blog',
   description:
     'En esta página web puedes crear blogs, editarlos y eliminarlos de mi blog personal que se encuentra en andrepg.vercel.app',
 };
